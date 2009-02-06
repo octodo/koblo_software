92e71746d6b3dcf2b989ba9b0559de9112645b4c
Lasse Steen Bohnstedt <lassesteenbohnstedt@lsb-macbook-osx.local>
2009-02-06 15:18:53 +0100

    KS: Cosmetic; a comment was wrong

--- Products/Koblo_Studio/trunk/include/DSP/CSample_DSP.h
+++ Products/Koblo_Studio/trunk/include/DSP/CSample_DSP.h
@@ -13,7 +13,7 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with the Koblo Stools. If not, see <http://www.gnu.org/licenses/>.
+// along with the Koblo SDK. If not, see <http://www.gnu.org/licenses/>.
 
 //!!! TO ASK is there always only one channel in a sound?
 //#define MAX_CHANNELS_IN_SOUND 0
--- Products/Koblo_Studio/trunk/include/Track_View/CTrack_Fade_Handle.h
+++ Products/Koblo_Studio/trunk/include/Track_View/CTrack_Fade_Handle.h
@@ -13,7 +13,7 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with the Koblo Stools. If not, see <http://www.gnu.org/licenses/>.
+// along with the Koblo SDK. If not, see <http://www.gnu.org/licenses/>.
 
 //class CTrack_Time_Pos;
 
